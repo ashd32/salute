2860d80939c141e53c0bf28fbc69ec5e7be2c3fb
ashd32 <ashd32@gmail.com>
2018-11-27 15:16:23 -0500

    reversed output

--- public/js/salute.js
+++ public/js/salute.js
@@ -1,50 +1,164 @@
+// $(function() {
+//   $.get("/api/users").then(function(users) {
+//     console.log(users);
+//     for (let i = 0; i < users.length; i++) {
+//       $(".kudo-from").append(
+//         "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+//       );
+//       $(".kudo-to").append(
+//         "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+//       );
+//     }
+//   });
+
+//   $(function() {
+//     $.get("/api/kudos").then(function(kudos) {
+//       console.log(kudos);
+//       for (let i = 0; i < kudos.length; i++) {
+          
+//         $(".kudos").append(
+//           "Title: <option value=" + kudos[i]._id + ">" + kudos[i].title + "</option>"
+//         );
+//         $(".kudos").append(
+//           "Message: <option value=" + kudos[i]._id + ">" + kudos[i].message + "</option>"
+//         );
+//       }
+//     });
+
+//     //Render the kudos on the index.html
+//     const renderKudos = function() {
+//       $.get("/api/kudos").then(function(kudosData) {
+//         kudosData.forEach(kudos => {
+//         });
+//       });
+//     };
+
+//     //Send Kudos to the server.
+//     const sendKudo = function() {
+//       const _fromId = $(".kudo-from")
+//         .val()
+//         .trim();
+//       const _toId = $(".kudo-to")
+//         .val()
+//         .trim();
+//       const title = $("#kudo-title")
+//         .val()
+//         .trim();
+//       const message = $("#kudo-body")
+//         .val()
+//         .trim();
+//       const noData = $(
+//         `<div class="alert alert-danger">_fromId, _toId, title, or/and message is/are missing. Please check the input data.</div>`
+//       );
+//       const duplicateUser = $(
+//         `<div class="alert alert-danger">_fromId and _toId cannot be the same.</div>`
+//       );
+
+//       console.log(_fromId, _toId, title, message);
+
+
+//       //New kudos to send to a server and then go on to the DB
+//       const newKudos = {
+//         title: title,
+//         message: message,
+//         from_user: _fromId,
+//         to_user: _toId
+//       };
+
+//       $.post("/api/kudos", newKudos).then(function(response) {
+//         console.log(response);
+//       });
+
+//       $("#kudoModal").removeClass("show");
+//       location.reload();
+//     };
+
+//     renderKudos();
+
+//     $("#send-kudo").click(sendKudo);
+//   });
+// });
+
+
+
+
+
+
+
+
 $(function() {
-  $.get("/api/users").then(function(users) {
+  $.get('/api/users').then(function(users) {
     console.log(users);
     for (let i = 0; i < users.length; i++) {
-      $(".kudo-from").append(
-        "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+      $('.kudo-from').append(
+        '<option value=' + users[i]._id + '>' + users[i].name + '</option>'
       );
-      $(".kudo-to").append(
-        "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+      $('.kudo-to').append(
+        '<option value=' + users[i]._id + '>' + users[i].name + '</option>'
       );
     }
   });
 
   $(function() {
-    $.get("/api/kudos").then(function(kudos) {
+    $.get('/api/kudos').then(function(kudos) {
       console.log(kudos);
+      /*
+      kudos object looks like: {
+        _id: ,
+        title: ,
+        message: ,
+        _fromId: ,
+        _toId:
+      }
+      */
       for (let i = 0; i < kudos.length; i++) {
-          
-        $(".kudos").append(
-          "Title: <option value=" + kudos[i]._id + ">" + kudos[i].title + "</option>"
-        );
-        $(".kudos").append(
-          "Message: <option value=" + kudos[i]._id + ">" + kudos[i].message + "</option>"
+        // $('.kudos').append(
+        //   'Title: <option value=' +
+        //     kudos[i]._id +
+        //     '>' +
+        //     kudos[i].title +
+        //     '</option>'
+        // );
+        // $(".kudos").append(
+        //   "Message: <option value=" + kudos[i]._id + ">" + kudos[i].message + "</option>"
+        // );
+        $('#kudosList').prepend(
+          '<div class="card-vody kudos" style="border: 3px solid black">' +
+            '<p>From: ' +
+            // kudosList[i]._fromId.name +
+            '</p><p>To: ' +
+            // kudos[i].toId.name +
+            '<p>Title: ' +
+            kudos[i].title +
+            '</p><p>Message: ' +
+            kudos[i].message +
+            '</div>'
         );
       }
+      console.log(kudos);
     });
 
     //Render the kudos on the index.html
-    const renderKudos = function() {
-      $.get("/api/kudos").then(function(kudosData) {
-        kudosData.forEach(kudos => {
-        });
-      });
-    };
+    //Don't need this renderKudos function as location.reload takes care of this
+    // const renderKudos = function() {
+    //   $.get('/api/kudos').then(function(kudosData) {
+    //     kudosData.forEach(kudos => {});
+    //   });
+    // };
 
     //Send Kudos to the server.
-    const sendKudo = function() {
-      const _fromId = $(".kudo-from")
+    const sendKudo = function(event) {
+      event.preventDefault();
+      const _fromId = $('.kudo-from')
         .val()
         .trim();
-      const _toId = $(".kudo-to")
+      const _toId = $('.kudo-to')
         .val()
         .trim();
-      const title = $("#kudo-title")
+      const title = $('#kudo-title')
         .val()
         .trim();
-      const message = $("#kudo-body")
+      const message = $('#kudo-body')
         .val()
         .trim();
       const noData = $(
@@ -54,27 +168,29 @@ $(function() {
         `<div class="alert alert-danger">_fromId and _toId cannot be the same.</div>`
       );
 
-      console.log(_fromId, _toId, title, message);
+      console.log('here');
 
+      console.log(_fromId, _toId, title, message);
 
       //New kudos to send to a server and then go on to the DB
       const newKudos = {
         title: title,
         message: message,
-        from_user: _fromId,
-        to_user: _toId
+        _fromId: _fromId,
+        _toId: _toId
       };
 
-      $.post("/api/kudos", newKudos).then(function(response) {
-        console.log(response);
+      $.post('/api/kudos', newKudos).then(function(response) {
+        $('#kudoModal').removeClass('show');
+        location.reload();
       });
 
-      $("#kudoModal").removeClass("show");
-      location.reload();
+      // $('#kudoModal').removeClass('show');
+      // location.reload();
     };
 
-    renderKudos();
+    // renderKudos();
 
-    $("#send-kudo").click(sendKudo);
+    $('#send-kudo').click(sendKudo);
   });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,70 @@
-const router = require("express").Router();
-const db = require("../models");
-  router.get("/api/kudos",function(req,res){
-  db.Kudos.find(function(err,kudos){
+// const router = require("express").Router();
+// const db = require("../models");
+//   router.get("/api/kudos",function(req,res){
+//   db.Kudos.find(function(err,kudos){
+//       res.json(kudos);
+//   });
+// });
+// router.post("/api/users", function(req,res){
+//   db.User.create(req.body).then(function(newUser){
+//     res.json({
+//       message:"userCreated"
+//     });
+//   });
+// });
+// router.post("/api/kudos", function(req,res){
+//   db.Kudos.create(req.body).then(function(newKudo){
+//     res.json({
+//       message:"kudoCreated"
+//     });
+//   });
+// });
+// router.get("/api/users", function(req,res){
+//   db.User.find().then(function(users){
+//     res.json(users);
+//   });
+// });
+// module.exports = router;
+
+
+
+
+
+const router = require('express').Router();
+const db = require('../models');
+router.get('/api/kudos', function(req, res) {
+  // db.Kudos.find(function(err,kudos){
+  //     res.json(kudos);
+  // });
+  //Updated find route so sender/receiver are populated in response
+  db.Kudos.find({})
+    .populate('_toId')
+    .populate('_fromId')
+    .then(function(kudos) {
       res.json(kudos);
-  });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
-router.post("/api/users", function(req,res){
-  db.User.create(req.body).then(function(newUser){
+router.post('/api/users', function(req, res) {
+  db.User.create(req.body).then(function(newUser) {
     res.json({
-      message:"userCreated"
+      message: 'userCreated'
     });
   });
 });
-router.post("/api/kudos", function(req,res){
-  db.Kudos.create(req.body).then(function(newKudo){
+router.post('/api/kudos', function(req, res) {
+  db.Kudos.create(req.body).then(function(newKudo) {
     res.json({
-      message:"kudoCreated"
+      message: 'kudoCreated'
     });
   });
 });
-router.get("/api/users", function(req,res){
-  db.User.find().then(function(users){
+router.get('/api/users', function(req, res) {
+  db.User.find().then(function(users) {
     res.json(users);
   });
 });
 module.exports = router;
+
