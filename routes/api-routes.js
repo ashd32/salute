92852beb3beed9915f62f8f9e9b7704ed69bec01
ashd32 <ashd32@gmail.com>
2018-11-27 02:19:25 -0500

    made kudos post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,27 @@
-const router = require("express").Router()
+const router = require("express").Router();
 const db = require("../models");
   router.get("/api/kudos",function(req,res){
   db.Kudos.find(function(err,kudos){
-      res.json(kudos)
+      res.json(kudos);
   });
 });
 router.post("/api/users", function(req,res){
   db.User.create(req.body).then(function(newUser){
     res.json({
       message:"userCreated"
-    })
-  })
-})
+    });
+  });
+});
+router.post("/api/kudos", function(req,res){
+  db.Kudos.create(req.body).then(function(newKudo){
+    res.json({
+      message:"kudoCreated"
+    });
+  });
+});
 router.get("/api/users", function(req,res){
   db.User.find().then(function(users){
-    res.json(users)
-  })
-})
+    res.json(users);
+  });
+});
 module.exports = router;
