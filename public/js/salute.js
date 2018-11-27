03c296721b67fe473a3150bc31b97ba154274b1b
ashd32 <ashd32@gmail.com>
2018-11-27 01:16:03 -0500

    added user functionality

--- public/js/salute.js
+++ public/js/salute.js
@@ -1,4 +1,8 @@
-$.get("/api/kudos").then(function(kudos){
+$.get("/api/users").then(function(users){
     console.log("test");
-    console.log(kudos);
+    console.log(users);
+    for (let i=0; i<users.length; i++){
+        $('.kudo-from').append('<option value='+users[i]._id+'>'+users[i].name+'</option>');
+        $('.kudo-to').append('<option value='+users[i]._id+'>'+users[i].name+'</option>');
+    }
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,4 +5,16 @@ const db = require("../models");
       res.json(kudos)
   });
 });
+router.post("/api/users", function(req,res){
+  db.User.create(req.body).then(function(newUser){
+    res.json({
+      message:"userCreated"
+    })
+  })
+})
+router.get("/api/users", function(req,res){
+  db.User.find().then(function(users){
+    res.json(users)
+  })
+})
 module.exports = router;
