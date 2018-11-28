f93c5a39db927ea278de51ce17318ae6392f6a62
ashd32 <ashd32@gmail.com>
2018-11-28 14:37:20 -0500

    fixed saulute.js

--- public/js/salute.js
+++ public/js/salute.js
@@ -1,31 +1,41 @@
 $(function() {
-  console.log("ready!");
-});
-$(function() {
-  $.get("/api/users").then(function(users) {
-    console.log(users);
-    // Appends users to dropdown menu in modal.
-    for (let i = 0; i < users.length; i++) {
-      $(".kudo-from").append(
-        "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
-      );
-      $(".kudo-to").append(
-        "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
-      );
-    }
-  });
+  const pageReady = function() {
+    // console.log("ready!");
+  };
+  
+
+  pageReady();
+
+  const getUsers = function() {
+    $.get("/api/users").then(function(users) {
+      // console.log(users);
+      // Appends users to dropdown menu in modal.
+      for (let i = 0; i < users.length; i++) {
+        $(".kudo-from").append(
+          "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+        );
+        $(".kudo-to").append(
+          "<option value=" + users[i]._id + ">" + users[i].name + "</option>"
+        );
+      }
+    });
+  };
+
+  
+
   // Gets the kudos
-  $(function() {
+  const getKudos = function() {
     $.get("/api/kudos").then(function(kudos) {
-      console.log(kudos);
+      // console.log(kudos);
 
       for (let i = 0; i < kudos.length; i++) {
+        console.log(kudos[i]._fromId.name, kudos[i]._toId.name, kudos[i].title, kudos[i].message);
         $("#kudosList").prepend(
           '<div class="card-vody kudos" style="border: 3px solid black">' +
             "<p>From: " +
-            // kudosList[i]._fromId.name +    // DOES NOT WORK
+            kudos[i]._fromId.name +    
             "</p><p>To: " +
-            // kudos[i].toId.name +           // DOES NOT WORK
+            kudos[i]._toId.name +           
             "<p>Title: " +
             kudos[i].title +
             "</p><p>Message: " +
@@ -34,47 +44,55 @@ $(function() {
         );
       }
     });
+  };
 
-    //Send Kudos to the server.
-    const sendKudo = function(event) {
-      event.preventDefault();
-      const _fromId = $(".kudo-from")
-        .val()
-        .trim();
-      const _toId = $(".kudo-to")
-        .val()
-        .trim();
-      const title = $("#kudo-title")
-        .val()
-        .trim();
-      const message = $("#kudo-body")
-        .val()
-        .trim();
-      const noData = $(
-        `<div class="alert alert-danger">_fromId, _toId, title, or/and message is/are missing. Please check the input data.</div>`
-      );
-      const duplicateUser = $(
-        `<div class="alert alert-danger">_fromId and _toId cannot be the same.</div>`
-      );
-      // Not console logging
-      console.log("here");
-      // Not console logging
-      console.log(_fromId, _toId, title, message);
+  //Send Kudos to the server.
+  const sendKudo = function(event) {
+    event.preventDefault();
+    const _fromId = $(".kudo-from")
+      .val()
+      .trim();
 
-      //New kudos to send to a server and then go on to the DB
-      const newKudos = {
-        title: title,
-        message: message,
-        _fromId: _fromId,
-        _toId: _toId
-      };
+    const _toId = $(".kudo-to")
+      .val()
+      .trim();
 
-      $.post("/api/kudos", newKudos).then(function(response) {
-        $("#kudoModal").removeClass("show");
-        location.reload();
-      });
+    const title = $("#kudo-title")
+      .val()
+      .trim();
+
+    const message = $("#kudo-body")
+      .val()
+      .trim();
+
+    const noData = $(
+      `<div class="alert alert-danger">_fromId, _toId, title, or/and message is/are missing. Please check the input data.</div>`
+    );
+
+    const duplicateUser = $(
+      `<div class="alert alert-danger">_fromId and _toId cannot be the same.</div>`
+    );
+
+
+
+    //New kudos to send to a server and then go on to the DB
+    const newKudos = {
+      title: title,
+      message: message,
+      _fromId: _fromId,
+      _toId: _toId
     };
-    // Submit button event listener
-    $("#send-kudo").click(sendKudo);
-  });
+
+    $.post("/api/kudos", newKudos).then(function(response) {
+      $("#kudoModal").removeClass("show");
+      location.reload();
+    });
+  };
+
+  // Submit button event listener
+  $("#send-kudo").click(sendKudo);
+
+
+  getUsers();
+  getKudos();
 });
